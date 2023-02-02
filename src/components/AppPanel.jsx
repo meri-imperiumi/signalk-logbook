ad4813d262e28ce6c85f03c48ac7453b9ec541f5
Henri Bergius <henri.bergius@iki.fi>
2023-02-02 16:07:40 +0100

    PoC for loading data from the server API

--- src/components/AppPanel.jsx
+++ src/components/AppPanel.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default class AppPanel extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
+function AppPanel() {
+  const [data, setData] = useState({
+    entries: [],
+  });
+
+  useEffect(() => {
+    fetch('/plugins/signalk-logbook/logs')
+      .then((res) => res.json())
+      .then((days) => {
+        const toShow = days.slice(-7);
+        Promise.all(toShow.map((day) => fetch(`/plugins/signalk-logbook/logs/${day}`)
+          .then((r) => r.json())))
+          .then((dayEntries) => {
+            const entries = [].concat.apply([], dayEntries); // eslint-disable-line prefer-spread
+            setData({
+              entries,
+            });
+          });
+      });
+  });
+
+  return (
+    <div>
+      <ul>
+      {data.entries.map((item) => (
+        <li key={item.datetime}>
+        <a href={item.datetime}>{item.text}</a>
+        </li>
+      ))}
+      </ul>
+    </div>
+  );
 }
+
+export default AppPanel;
